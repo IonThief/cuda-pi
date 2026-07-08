1272c6947c5c0c69d365fdb3c86d101a5a8463e1
ibrahim <ibrahim.sayed@edi.lv>
2026-07-08 15:55:47 +0300

    rename 'calculate_pi' to just 'calculate'

--- inc/pi_reduce.cuh
+++ inc/pi_reduce.cuh
@@ -7,6 +7,6 @@ constexpr int THREADS_PER_BLOCK = 256;
 __global__ void reduce_pi_kernel(double* d_partial_sums,
                                  unsigned long long num_intervals);
 
-double calculate_pi(unsigned long long num_intervals);
+double calculate(unsigned long long num_intervals);
 
 }  // namespace cuda_pi
