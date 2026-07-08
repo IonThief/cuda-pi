cedece9f8e2de94f0809d7d081c7a772d38c4f5a
ibrahim <ibrahim.sayed@edi.lv>
2026-07-08 15:57:49 +0300

    add impl of calculate function

--- src/pi_reduce.cu
+++ src/pi_reduce.cu
@@ -1,5 +1,8 @@
 #include <cuda_runtime.h>
 
+#include <numeric>
+#include <vector>
+
 #include "pi_reduce.cuh"
 
 namespace cuda_pi {
@@ -34,6 +37,30 @@ __global__ void reduce_pi_kernel(double* d_partial_sums,
   if (tid == 0) {
     d_partial_sums[blockIdx.x] = sdata[0];
   }
+}
+
+double calculate(unsigned long long num_intervals) {
+  int num_blocks = 512;
+  size_t alloc_size = num_blocks * sizeof(double);
+
+  std::vector<double> h_partial_sums(num_blocks);
+  double* d_partial_sums = nullptr;
+
+  cudaMalloc(&d_partial_sums, alloc_size);
+
+  reduce_pi_kernel<<<num_blocks, THREADS_PER_BLOCK>>>(d_partial_sums,
+                                                      num_intervals);
+  cudaGetLastError();
+  cudaDeviceSynchronize();
+  cudaMemcpy(h_partial_sums.data(), d_partial_sums, alloc_size,
+             cudaMemcpyDeviceToHost);
+  cudaFree(d_partial_sums);
+
+  double total_sum =
+      std::accumulate(h_partial_sums.begin(), h_partial_sums.end(), 0.0);
+
+  double step = 1.0 / static_cast<double>(num_intervals);
+  return total_sum * step * 4.0;
 }
 
 }  // namespace cuda_pi
