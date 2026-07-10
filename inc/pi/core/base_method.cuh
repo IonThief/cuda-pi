cfaae562f2100770960669b993a216c384e27fe6
ibrahim <ibrahim.sayed@edi.lv>
2026-07-10 16:30:23 +0300

    add compute_impl() method to wrap it with timing code

--- inc/pi/core/base_method.cuh
+++ inc/pi/core/base_method.cuh
@@ -2,12 +2,14 @@
 #include "pi/core/types.cuh"
 
 namespace pi::core {
-
 class BaseMethod {
  public:
   virtual ~BaseMethod() = default;
 
-  virtual PiResult compute() = 0;
+  PiResult compute();
+
+ protected:
+  virtual PiResult compute_impl() = 0;
 };  // class BaseMethod
 
 }  // namespace pi::core
