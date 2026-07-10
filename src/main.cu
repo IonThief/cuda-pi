1f2e09c721bbec93a54dc685fe61861144bb5299
ibrahim <ibrahim.sayed@edi.lv>
2026-07-10 16:32:52 +0300

    update main to use the new structure, to use Riemann method

--- src/main.cu
+++ src/main.cu
@@ -1,27 +1,17 @@
-#include <chrono>
-#include <iomanip>
 #include <iostream>
 
-#include "pi_reduce.cuh"
+#include "pi/method/quadrature/riemann.cuh"
 
-int main(int argc, char* argv[]) {
-  unsigned long long num_intervals = 100'000'000;
+int main() {
+  unsigned long long intervals = 1'000'000'000ULL;
+  pi::method::Riemann method(intervals);
 
-  if (argc > 1) {
-    num_intervals = std::stoull(argv[1]);
-  }
+  pi::core::PiResult result = method.compute();
 
-  std::cout << "Calculating Pi using " << num_intervals << " intervals..."
-            << std::endl;
-
-  auto start_time = std::chrono::high_resolution_clock::now();
-  double pi = cuda_pi::calculate(num_intervals);
-  auto end_time = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> duration = end_time - start_time;
-
-  std::cout << std::fixed << std::setprecision(10);
-  std::cout << "Calculated Pi: " << pi << std::endl;
-  std::cout << "Time taken   : " << duration.count() << " ms" << std::endl;
+  std::cout << "Algorithm: Riemann Sum\n";
+  std::cout << "Result:    " << result.digits << "\n";
+  std::cout << "Time:      " << result.compute_time_ms << " ms\n";
+  std::cout << "Steps:     " << result.iterations << "\n";
 
   return 0;
 }
