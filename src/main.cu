13bf347aa19f5c9970c2fe3131960b802f74e896
ibrahim <ibrahim.sayed@edi.lv>
2026-07-13 18:02:13 +0300

    update main with a helper function to print result for a selected method, to make it easy to compare methods

--- src/main.cu
+++ src/main.cu
@@ -1,17 +1,35 @@
+#include <iomanip>
 #include <iostream>
+#include <string>
 
 #include "pi/method/quadrature/riemann.cuh"
+#include "pi/method/stochastic/monte_carlo.cuh"
 
-int main() {
-  unsigned long long intervals = 1'000'000'000ULL;
-  pi::method::Riemann method(intervals);
+void print_results(const std::string& name, pi::core::BaseMethod& method) {
+  std::cout << "Algorithm: " << name << "\n";
 
   pi::core::PiResult result = method.compute();
 
-  std::cout << "Algorithm: Riemann Sum\n";
-  std::cout << "Result:    " << result.digits << "\n";
-  std::cout << "Time:      " << result.compute_time_ms << " ms\n";
-  std::cout << "Steps:     " << result.iterations << "\n";
+  const double TRUE_PI = 3.14159265358979323846;  // from Wikipedia
+  double error = std::abs(result.value - TRUE_PI);
+
+  std::cout << std::fixed << std::setprecision(15);
+  std::cout << "Result:   " << result.value << "\n";
+  std::cout << "Error:    " << error << "\n";
+  std::cout << std::setprecision(3);
+  std::cout << "Time:     " << result.compute_time_ms << " ms\n";
+  std::cout << "Steps:    " << result.iterations << "\n";
+  std::cout << "--------------------------------------\n";
+}
+
+int main() {
+  unsigned long long intervals = 1'000'000'000ULL;
+
+  pi::method::Riemann riemann(intervals);
+  print_results("Riemann", riemann);
+
+  pi::method::MonteCarlo monte_carlo(intervals);
+  print_results("Monte Carlo", monte_carlo);
 
   return 0;
 }
