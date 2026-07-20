e61ddd17c727df1783f7dae146f60be4eba2c4e5
ibrahim <ibrahim.sayed@edi.lv>
2026-07-20 17:07:00 +0300

    added data structure for BigFloat and helper functions (allocate and free)

--- inc/pi/core/types.cuh
+++ inc/pi/core/types.cuh
@@ -1,4 +1,8 @@
 #pragma once
+#include <cuda_runtime.h>
+
+#include <cstdint>
+#include <cstdio>
 
 namespace pi::core {
 struct PiResult {
@@ -6,4 +10,28 @@ struct PiResult {
   double compute_time_ms;
   int iterations;
 };  // struct PiResult
+
+struct BigFloat {
+  int sign;
+  long long exponent;
+  size_t num_limbs;
+  uint32_t* limbs;
+};  // struct BigFloat
+
+inline void allocate_big_float(BigFloat* bf, size_t num_limbs) {
+  bf->sign = 1;  // 1 for +ve, -1 for -ve
+  bf->exponent = 0;
+  bf->num_limbs = num_limbs;
+
+  cudaError_t err =
+      cudaMallocManaged(&(bf->limbs), num_limbs * sizeof(uint32_t));
+  if (err != cudaSuccess) printf("CUDA Error: %s\n", cudaGetErrorString(err));
+}
+
+inline void free_big_float(BigFloat* bf) {
+  if (bf->limbs) {
+    cudaFree(bf->limbs);
+    bf->limbs = nullptr;
+  }
+}
 }  // namespace pi::core
